c08045fb52581a0ff3f37c9476922951557136ea
ZharfanDS <zharfan@rocketmail.com>
2021-10-24 16:47:07 +0700

    menambahkan fitur webcam dan ada kesalahan pada fitur LPND

--- LPND.py
+++ LPND.py
@@ -11,13 +11,40 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = cv2.bilateralFilter(gray, 13, 15, 15) 
 cv2.imshow("ASDSA",gray)
 
+img_blur = cv2.GaussianBlur(gray, (3,3), 0)
+
+ 
+
+# Sobel Edge Detection
+
+sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5) # Sobel Edge Detection on the X axis
+
+sobely = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5) # Sobel Edge Detection on the Y axis
+
+sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5) # Combined X and Y Sobel Edge Detection
+
+# Display Sobel Edge Detection Images
+
+cv2.imshow('Sobel X', sobelx)
+
+cv2.waitKey(0)
+
+cv2.imshow('Sobel Y', sobely)
+
+cv2.waitKey(0)
+
+cv2.imshow('Sobel X Y using Sobel() function', sobelxy)
+cv2.waitKey(0)
+
+
+# ASDSAD
 cv2.waitKey(0)
 edged = cv2.Canny(gray, 100, 200) 
 cv2.imshow("ASDSA",edged)
 
 cv2.waitKey(0)
-ret, thresh = cv2.threshold(edged.copy(), 200, 255, cv2.THRESH_BINARY)
-contours = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+# ret, thresh = cv2.threshold(sobelxy, 200, 255, cv2.THRESH_BINARY)
+contours = cv2.findContours(sobelxy, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 contours = imutils.grab_contours(contours)
 contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
 screenCnt = None
