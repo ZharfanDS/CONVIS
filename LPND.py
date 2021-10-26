2690bb3cd2ddc7153caea628bb01b7ae5b5a3975
ZharfanDS <zharfan@rocketmail.com>
2021-10-26 16:01:42 +0700

    perubahan egde

--- LPND.py
+++ LPND.py
@@ -1,50 +1,18 @@
+#@programming_fever
 import cv2
 import imutils
 import numpy as np
 import pytesseract
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
 
-img = cv2.imread("D:\Zharfan File\OneDrive\OneDrive - Bina Nusantara\Documents\S__2359329.jpg",cv2.IMREAD_COLOR)
+img = cv2.imread('C:\Program Files\Tesseract-OCR\tesseract.exe',cv2.IMREAD_COLOR)
 img = cv2.resize(img, (600,400) )
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
 gray = cv2.bilateralFilter(gray, 13, 15, 15) 
-cv2.imshow("ASDSA",gray)
 
-img_blur = cv2.GaussianBlur(gray, (3,3), 0)
-
- 
-
-# Sobel Edge Detection
-
-sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5) # Sobel Edge Detection on the X axis
-
-sobely = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5) # Sobel Edge Detection on the Y axis
-
-sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5) # Combined X and Y Sobel Edge Detection
-
-# Display Sobel Edge Detection Images
-
-cv2.imshow('Sobel X', sobelx)
-
-cv2.waitKey(0)
-
-cv2.imshow('Sobel Y', sobely)
-
-cv2.waitKey(0)
-
-cv2.imshow('Sobel X Y using Sobel() function', sobelxy)
-cv2.waitKey(0)
-
-
-# ASDSAD
-cv2.waitKey(0)
-edged = cv2.Canny(gray, 100, 200) 
-cv2.imshow("ASDSA",edged)
-
-cv2.waitKey(0)
-# ret, thresh = cv2.threshold(sobelxy, 200, 255, cv2.THRESH_BINARY)
-contours = cv2.findContours(sobelxy, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+edged = cv2.Canny(gray, 30, 200) 
+contours = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 contours = imutils.grab_contours(contours)
 contours = sorted(contours, key = cv2.contourArea, reverse = True)[:10]
 screenCnt = None
@@ -52,8 +20,8 @@ screenCnt = None
 for c in contours:
     
     peri = cv2.arcLength(c, True)
-    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-    print(len(approx))
+    approx = cv2.approxPolyDP(c, 0.018 * peri, True)
+ 
     if len(approx) == 4:
         screenCnt = approx
         break
